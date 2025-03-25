421a965e6d1d874ef5f59bd812adb9e06c73b6b3
IqbalRadjasa <iqbal.radjasa@gmail.com>
2025-03-26 02:23:34 +0700

    Add(Booking):Penambahan Pembayaran dan Responsifitas

--- asset/js/booking.js
+++ asset/js/booking.js
@@ -2,4 +2,21 @@ $(document).ready(function () {
   $('.pelayanan-item').click(function () {
     window.location.href = 'booking-2.html';
   });
+
+  $('#selesai-booking-btn').click(function (e) {
+    e.preventDefault();
+
+    let pembayaran_val = $('#pembayaran-input').val();
+
+    if (pembayaran_val == 1) {
+      window.location.href = 'bukti-transfer.html';
+    } else if (pembayaran_val == 2) {
+      window.location.href = 'bukti-qris.html';
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Pilih metode pembayaran.',
+      });
+    }
+  });
 });
